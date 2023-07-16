e8084cdaf64066c8c3e03e0bcbf147735f8f59b2
SrishtiC99 <iamsri9631@gmail.com>
2023-07-16 16:17:43 +0530

    first commit-all services running

---
+++ alice-service/src/main/java/com/srishti/aliceservice/AliceServiceApplication.java
@@ -0,0 +1,12 @@
+package com.srishti.aliceservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AliceServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AliceServiceApplication.class, args);
+    }
+}
---
+++ bob-service/src/main/java/com/srishti/bobservice/BobServiceApplication.java
@@ -0,0 +1,11 @@
+package com.srishti.bobservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BobServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BobServiceApplication.class, args);
+    }
+}
